1133bf81653ed9b10c50fa8c9a60e383db920e0f
GummiCode <gummicodeblog@gmail.com>
2020-07-05 19:27:34 +0100

    Cleared the React app of unneccessary elements. Set the page's title.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+      ʕ⊙ᴥ⊙ʔ
+      </p>
+       
+    </div>
+  );
+}
+
+export default App;
