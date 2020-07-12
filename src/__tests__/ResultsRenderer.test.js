e71a15c54056bc2a15a422a84b4e7adfe6ef495c
GummiCode <gummicodeblog@gmail.com>
2020-07-12 23:14:08 +0100

    Added a render snapshot test for the ResultsRenderer component, with mock searchResults data.

---
+++ src/__tests__/ResultsRenderer.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ResultsRenderer from '../components/ResultsRenderer';
+
+const mockResults = {
+  images: [
+    {
+      id: "id1",
+      image: "https://mockaddress.mock/1",
+      description: "Mock1"
+    },
+    {
+      id: "id2",
+      image: "https://mockaddress.mock/2",
+      description: "Mock2"
+    },
+    {
+      id: "id3",
+      image: "https://mockaddress.mock/3",
+      description: "Mock3"
+    },
+  ]
+}
+
+test('component ResultsRenderer renders', () => {
+  const { asFragment } = render(<ResultsRenderer searchResults={mockResults} />);
+  const component = asFragment();
+  expect (component).toMatchSnapshot();
+});
