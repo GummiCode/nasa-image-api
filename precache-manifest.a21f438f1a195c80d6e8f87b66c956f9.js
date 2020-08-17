0b89313e60aa4cf10e947eb7b766667b32780599
GummiCode <gummicodeblog@gmail.com>
2020-08-17 03:13:39 +0100

    Updates

---
+++ precache-manifest.a21f438f1a195c80d6e8f87b66c956f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e816bf825d32708d78fd41f861ecc0",
+    "url": "/nasa-image-search/index.html"
+  },
+  {
+    "revision": "ecf93b575612cda4e003",
+    "url": "/nasa-image-search/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ecf93b575612cda4e003",
+    "url": "/nasa-image-search/static/js/2.709f37ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-image-search/static/js/2.709f37ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d60e1d184a5a26a36f7",
+    "url": "/nasa-image-search/static/js/main.a9b5efe9.chunk.js"
+  },
+  {
+    "revision": "13dfbb56909a3a70c8fe",
+    "url": "/nasa-image-search/static/js/runtime-main.d96ef597.js"
+  }
+]);
