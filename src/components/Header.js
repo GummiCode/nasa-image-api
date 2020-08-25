58592421b752129c0f8d4ff182af0fc69c4aa9cb
GummiCode <43494071+GummiCode@users.noreply.github.com>
2020-08-25 20:58:53 +0100

    After experiencing broken images on local devices but not on deployed repos of the same version, I've started hosting images in assets folders within the src directory. Created this directory in this repo. Moved NASA header image to the assets directory. Imported to Header. Renders correctly.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import HeaderImage from './HeaderImage';
 import SearchInput from './SearchInput';
+import nasaLogo from '../assets/images/nasa.png';
 
 const HeaderStyles = styled.div`
   height: 60px;
@@ -16,6 +17,10 @@ const HeaderStyles = styled.div`
   flex-flow: row nowrap;
   justify-content: start;
   align-items: center;
+
+  @media (max-width: 640px) {
+    height: 50px;
+  }
 `;
 
 const HeaderText = styled.p`
@@ -23,13 +28,17 @@ const HeaderText = styled.p`
   width: 230px;
   font-size: 2.2em;
   color: red;
+
+  @media (max-width: 640px) {
+    font-size: 1.8em;
+  }
 `
 const Header = ({ setSearchResults, setResultsLoading }) => {
 
   return (
     <HeaderStyles>
       <HeaderImage 
-        src="./images/nasa.png" 
+        src={nasaLogo} 
         alt="NASA logo"
       />
       <HeaderText>Image Search</HeaderText>
--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
+
 const HeaderImage = styled.img`
   height: 30px;
   width: auto;
+
+  @media (max-width: 640px) {
+    height: 25px;
+  }
 `
 
 export default HeaderImage;
